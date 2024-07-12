25324e9d12bab4263c2caa1633d1746aef58f967
AngelSR3 <angelduvan1016@gmail.com>
2024-07-12 15:30:45 -0500

    feat: :rocket: Banner

--- modregistro.py
+++ modregistro.py
@@ -5,9 +5,9 @@ def registrarciudad():
     postal=input("Escriba el codigo postal de la ciudad: ")
     poblacion=input("Escriba la poblacion estimada de la ciudad: ")
     pais=input("Escriba el pais al que pertenece la ciudad: ")
-    ciudades=leerJson("git/ciudades")
+    ciudades=leerJson("ciudades")
     ciudades.append({"Nombre": nombre, "Codigo Postal": postal, "Poblacion Estimada": poblacion, "Pais": pais})
-    guardarJson("git/ciudades", ciudades)
+    guardarJson("ciudades", ciudades)
     
 def leerCiudades():
     opcion = input("""Ingrese que dato desea mostrar de las ciudades
@@ -15,7 +15,7 @@ def leerCiudades():
 Nombre | Postal | Poblacion | Pais
           
 Ingrese su opcion: """)
-    ciudades = leerJson("git/ciudades")
+    ciudades = leerJson("ciudades")
     for ciudad in ciudades:
         if opcion == "Nombre":
             print(ciudad["Nombre"])
