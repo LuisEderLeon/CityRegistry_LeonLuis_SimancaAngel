de0e4fbf955bcfeb956663b9104aaf2e01677a96
AngelSR3 <angelduvan1016@gmail.com>
2024-07-12 14:55:45 -0500

    fix: :hammer: Guardar en json

--- modregistro.py
+++ modregistro.py
@@ -5,4 +5,8 @@ def registrarciudad():
     postal=input("Escriba el codigo postal de la ciudad: ")
     poblacion=input("Escriba la poblacion estimada de la ciudad: ")
     pais=input("Escriba el pais al que pertenece la ciudad: ")
+    ciudades=[]
+    ciudades.append({"Nombre": nombre, "Codigo Postal": postal, "Poblacion Estimada": poblacion, "Pais": pais})
+    guardarJson("ciudades", ciudades)
     
+
