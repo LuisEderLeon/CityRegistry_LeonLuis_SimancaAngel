87f5ca9a71b1712c0d7917f2082e0762b3d887d6
AngelSR3 <angelduvan1016@gmail.com>
2024-07-12 15:14:06 -0500

    fix: :hammer: Arreglo en funciones

--- modregistro.py
+++ modregistro.py
@@ -5,13 +5,17 @@ def registrarciudad():
     postal=input("Escriba el codigo postal de la ciudad: ")
     poblacion=input("Escriba la poblacion estimada de la ciudad: ")
     pais=input("Escriba el pais al que pertenece la ciudad: ")
-    ciudades=[]
+    ciudades=leerJson("git/ciudades")
     ciudades.append({"Nombre": nombre, "Codigo Postal": postal, "Poblacion Estimada": poblacion, "Pais": pais})
     guardarJson("git/ciudades", ciudades)
     
 def leerCiudades():
     input("""Ingrese 
           """)
-    ciudades = leerJson("ciudades")
+    ciudades = leerJson("git/ciudades")
     for ciudad in ciudades:
         print(ciudad)
+
+def editarciudades():
+    codigo=input("Escriba el codigo postal de la ciudad para editar su informacion: ")
+    
