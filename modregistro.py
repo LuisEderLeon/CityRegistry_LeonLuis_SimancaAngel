f0b0a47679a35cf10ba8c88604db6d1d247acaf2
AngelSR3 <angelduvan1016@gmail.com>
2024-07-12 14:50:02 -0500

    feat: :rocket: Registrar ciudades

---
+++ modregistro.py
@@ -0,0 +1,8 @@
+from leerJson import *
+
+def registrarciudad():
+    nombre = input("Escriba el nombre de la ciudad: ")
+    postal=input("Escriba el codigo postal de la ciudad: ")
+    poblacion=input("Escriba la poblacion estimada de la ciudad: ")
+    pais=input("Escriba el pais al que pertenece la ciudad: ")
+    
